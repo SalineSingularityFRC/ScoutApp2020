2dfd1f9ab1b142921e24947ca43554f057779088
nhwltrs <nhwltrs>
2020-01-25 10:47:19 -0500

    Added the database methods in onStop for capabilities

--- app/src/main/java/com/example/nhwltrs/scoutapp2020/Capabilities.java
+++ app/src/main/java/com/example/nhwltrs/scoutapp2020/Capabilities.java
@@ -10,6 +10,11 @@ import android.widget.TextView;
 import android.widget.Button;
 
 public class Capabilities extends Fragment {
+
+    //Variables to send to the database
+    Counter upper;
+    Counter lower;
+
     public Capabilities() {
         // Required empty public constructor
     }
@@ -23,8 +28,10 @@ public class Capabilities extends Fragment {
             public void onClick(View v) {
                 if (collect.isChecked()) {
                     // DB foolishness
+                    DatabaseClass.setFloorCollection(true);
                 } else {
                     // More DB foolishness
+                    DatabaseClass.setFloorCollection(false);
                 }
             }
         });
@@ -34,15 +41,25 @@ public class Capabilities extends Fragment {
             public void onClick(View v) {
                 if (trench.isChecked()) {
                     // DB fiddling
+                    DatabaseClass.setTrench(true);
                 } else {
                     // DB absurdity
+                    DatabaseClass.setTrench(false);
                 }
             }
         });
 
-        Counter upper = new Counter((Button)view.findViewById(R.id.capUpperUp), (Button)view.findViewById(R.id.capUpperDown), (TextView)view.findViewById(R.id.capUpperDisplay));
-        Counter lower = new Counter((Button)view.findViewById(R.id.capLowerUp), (Button)view.findViewById(R.id.capLowerDown), (TextView)view.findViewById(R.id.capLowerDisplay));
+        upper = new Counter((Button)view.findViewById(R.id.capUpperUp), (Button)view.findViewById(R.id.capUpperDown), (TextView)view.findViewById(R.id.capUpperDisplay));
+        lower = new Counter((Button)view.findViewById(R.id.capLowerUp), (Button)view.findViewById(R.id.capLowerDown), (TextView)view.findViewById(R.id.capLowerDisplay));
 
         return view;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        DatabaseClass.setUpperBay(upper.get_count());
+        DatabaseClass.setLowerBay(lower.get_count());
     }
 }
