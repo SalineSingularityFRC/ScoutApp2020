1127453d36cab9da1355447502d9f1afc941d45f
Skye Bleed <monarrk@aaathats3as.com>
2020-02-24 18:49:19 -0500

    Remove add dead code + general cleanup

--- app/src/main/java/com/example/nhwltrs/scoutapp2020/MatchData.java
+++ app/src/main/java/com/example/nhwltrs/scoutapp2020/MatchData.java
@@ -3,14 +3,12 @@ package com.example.nhwltrs.scoutapp2020;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Handler;
-import android.os.SystemClock;
 import android.support.design.widget.TabLayout;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,40 +17,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MatchData extends AppCompatActivity {
-
-    private SectionsPagerAdapter mSectionsPagerAdapter;
     private ViewPager mViewPager;
-    long StartTime, MillisecondTime, TimeBuff, UpdateTime = 0L ;
     Handler handler;
-    //int Seconds;
-    //int MilliSeconds;
-    //TextView matchTimer;
     TextView teamDuringMatchTextView1;
     TextView teamDuringMatchTextView2;
     Button endMatch;
-    boolean started = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match_data);
 
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager = findViewById(R.id.container);
         setUpViewPager(mViewPager);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        TabLayout tabLayout = findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
-        // matchTimer  = (TextView) findViewById(R.id.matchTimer);
-        teamDuringMatchTextView1 = (TextView) findViewById(R.id.teamDuringMatchTextView1);
-        teamDuringMatchTextView2 = (TextView) findViewById(R.id.teamDuringMatchTextView2);
-        //final Button startMatch = (Button) findViewById(R.id.matchStartButton);
-        endMatch = (Button) findViewById(R.id.matchEndButton);
-        //endMatch.setVisibility(View.INVISIBLE);
+
+        // Team $NUMBER displays
+        teamDuringMatchTextView1 = findViewById(R.id.teamDuringMatchTextView1);
+        teamDuringMatchTextView2 = findViewById(R.id.teamDuringMatchTextView2);
+
+        // Send data
+        endMatch = (Button)findViewById(R.id.matchEndButton);
 
         handler = new Handler();
-        StartTime = SystemClock.uptimeMillis();
 
         if(getIntent().hasExtra("Team Number")) {
             String teamNumberString = getIntent().getExtras().toString();
@@ -62,16 +51,6 @@ public class MatchData extends AppCompatActivity {
             teamDuringMatchTextView1.setText(teamNumberString);
         }
 
-//        startMatch.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                startMatch.setVisibility(View.INVISIBLE);
-//                StartTime = SystemClock.uptimeMillis();
-//                handler.postDelayed(runnable, 0);
-//                started = true;
-//            }
-//        });
-
         endMatch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -80,24 +59,6 @@ public class MatchData extends AppCompatActivity {
             }
         });
     }
-
-//    public Runnable runnable = new Runnable() {
-//        public void run() {
-//            MillisecondTime = SystemClock.uptimeMillis() - StartTime;
-//            UpdateTime = TimeBuff + MillisecondTime;
-//            Seconds = (int) (UpdateTime / 1000);
-//            int timeLeft = 1 - Seconds;
-//            MilliSeconds = (int) (UpdateTime % 1000);
-//            MilliSeconds = MilliSeconds / 1000;
-//            if (timeLeft < 1) {
-//                matchTimer.setText("Match Over!");
-//                endMatch.setVisibility(View.VISIBLE);
-//            } else {
-//                matchTimer.setText(timeLeft + "");
-//            }
-//            handler.postDelayed(this, 0);
-//        }
-//    };
 
     private void setUpViewPager(ViewPager viewPager) {
         SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
@@ -138,7 +99,4 @@ public class MatchData extends AppCompatActivity {
             return mFragmentList.size();
         }
     }
-//    public int getTimer(){
-//        return Seconds;
-//    }
 }
